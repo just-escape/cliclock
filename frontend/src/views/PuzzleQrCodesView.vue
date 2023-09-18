734c358ded196b3dede9c735f543c050e4acdc09
Jean-Baptiste Braun <jbaptiste.braun@gmail.com>
2023-09-18 11:56:42 +0200

    fixes

--- frontend/src/views/PuzzleQrCodesView.vue
+++ frontend/src/views/PuzzleQrCodesView.vue
@@ -14,17 +14,17 @@ axios.get(url).then(({data}) => {
 </script>
 
 <template>
-<div class="container-fluid" size="A4" style="background: white">
+<div class="container-fluid" style="background: white">
     <div class="row h-100 w-100">
-        <div class="d-flex flex-column col-4 mb-4" v-for="puzzle in puzzles" :key="puzzle.slug">
-            <div class="d-flex justify-content-center">
+        <div class="d-flex flex-column col-12 pagebreak" v-for="puzzle in puzzles" :key="puzzle.slug">
+            <div class="d-flex justify-content-center mt-5">
                 <vue-qrcode
                 :value="'puzzle-' + puzzle.slug"
-                :options="{width: 150, margin: 0}"
+                :options="{width: 500, margin: 0}"
                 />
             </div>
             <div class="text-center" style="color: black">
-                {{ puzzle.slug }}
+                Nom de code : {{ puzzle.slug }}
             </div>
         </div>
     </div>
@@ -32,19 +32,10 @@ axios.get(url).then(({data}) => {
 </template>
 
 <style scoped>
-div[size="A4"] {
-  background: white;
-  width: 21cm;
-  height: 29.7cm;
-  display: block;
-  margin: 0 auto;
-  margin-bottom: 0.5cm;
-}
-
 @media print {
-  body, div[size="A4"] {
-    margin: 0;
-    box-shadow: 0;
-  }
+    .pagebreak {
+        clear: both;
+        page-break-after: always;
+    }
 }
 </style>
