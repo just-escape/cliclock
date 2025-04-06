87aef41bcfe507f0b16ba2ad56cb54dd93763664
Jean-Baptiste Braun <jbaptiste.braun@gmail.com>
2025-04-06 18:06:06 +0200

    give front and back cpu boost

--- infra/modules/instance/backoffice.tf
+++ infra/modules/instance/backoffice.tf
@@ -23,6 +23,7 @@ resource "google_cloud_run_v2_service" "backoffice" {
           cpu    = "1"
           memory = "512Mi"
         }
+        startup_cpu_boost = true
       }
 
       env {
--- infra/modules/instance/frontend.tf
+++ infra/modules/instance/frontend.tf
@@ -6,6 +6,14 @@ resource "google_cloud_run_v2_service" "frontend" {
     containers {
       image = "${var.gcp_region}-docker.pkg.dev/${var.project_id}/${data.terraform_remote_state.base.outputs.gar_name}/frontend:v1.0.4"
 
+      resources {
+        limits = {
+          "cpu" = "1"
+          "memory" = "512Mi"
+        }
+        startup_cpu_boost = true
+      }
+
       env {
         name = "BASE_URL"
         value = "https://${var.namespace}-backoffice-${data.google_project.project.number}.${var.gcp_region}.run.app"
